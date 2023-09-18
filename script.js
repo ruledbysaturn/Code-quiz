26e1227c40aa24e4cb31190878e199dc8a86e392
Ruminate <inramiland@gmail.com>
2023-09-17 22:59:16 -0400

    added questions to js

---
+++ script.js
@@ -0,0 +1,29 @@
+var questionLine = {
+    question: "Commonly used data types DO NOT include:"
+    choices: ["strings", "booleans", "alerts", "numbers"]
+    correctAnswer: 2
+}
+
+{
+    question: "The condition in an if/else statement is enclosed with _______."
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"]
+    correctAnswer: 2
+}
+
+{
+    question: "Arrays in Javascript can be used to store _____."
+    choices: ["numbers & strings", "other arrays", "booleans", "all of the above"]
+    correctAnswer: 3
+}
+
+{
+    question: "String values must be enclosed within _____ when being assigned to variables."
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"]
+    correctAnswer: 2
+}
+ {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:"
+    choices: ["Javascript", "terminal/bash", "for loops", "console.log"]
+    correctAnswer: 0
+}
+
