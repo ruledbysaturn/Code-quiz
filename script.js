45289dfde26568e7b1499f03f228d3d2b08ac5e6
Ruminate <inramiland@gmail.com>
2023-11-07 21:16:32 -0500

    function for checking answers/deducting time if wrong

--- script.js
+++ script.js
@@ -29,13 +29,15 @@ var questionLine = [
     correctAnswer: 0
 }]
 
-var currentQuestion = 0;
+var currentQuestionDisplay = 0;
 var score = 0;
 var timeLeft = 60;
 
 var startButton = document.getElementById('start-btn');
 var timerEl = document.getElementById('timer');
 var quizSection = document.querySelector('.quiz');
+
+//event listener and function for start button
 
 startButton.addEventListener("click", function () {
     startTimer();
@@ -47,9 +49,48 @@ function startTimer() {
         timeLeft--;
         timerEl.textContent = 'Time: ' + timeLeft;
         
-        if (timeLeft <= 0 || currentQuestion >= questionLine.length) {
+        if (timeLeft <= 0 || currentQuestionDisplay >= questionLine.length) {
             clearInterval(timer);
             endQuiz();
         }
     }, 1000);
+}
+
+function loadQuestion() {
+    quizSection.style.display = 'block';
+    startButton.style.display = 'none';
+    var currentQuestion = questionLine[currentQuestionDisplay];
+    var titleEl = document.getElementById('titel');
+    titleEl.textContent = currentQuestion.title;
+
+    var choicesEl = document.querySelectorAll('.choices');
+    for (var i = 0; i < choicesEl.length; i++) {
+        choicesEl[i].textContent = currentQuestion.choices[i];
+
+        choicesEl[i].addEventListener('click', function() {
+            var userAnswer = this.textContent;
+            checkAnswer(userAnswer, currentQuestion.correctAnswer);
+          });
+    }
+}
+
+function checkAnswer(userAnswer, correctAnswer) {
+    if (userAnswer === questionLine[currentQuestionDisplay].choices[correctAnswer]) {
+        score++;
+    }
+    else{
+        timeLeft -= 10;
+        if (timeLeft < 0) {
+            timeLeft = 0;
+        }
+        timerEl.textContent = 'Time: ' + timeLeft;
+    }
+
+    currentQuestionDisplay++;
+    if (currentQuestionDisplay < questionLine.length) {
+        loadQuestion();
+    } 
+    else {
+        endQuiz();
+    }
 }
